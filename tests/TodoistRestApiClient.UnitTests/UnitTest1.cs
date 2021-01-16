18258009101cfe03398fd525fb3983c4709822dd
timoheller99 <timo.heller@5minds.de>
2021-01-16 16:03:09 +0100

    add unit tests project

---
+++ tests/TodoistRestApiClient.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TodoistRestApiClient.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
